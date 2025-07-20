b24f72b74a024da5fb01d68b1b2326d55dd0d7c7
Alok Kumar <alokkumar012148@gmail.com>
2025-07-20 12:27:29 +0530

    Day 2 of Learning React Native, Learned the basics, setup project folder structure

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AppLayout = () => {
+  return (
+    <View>
+      <Text>AppLayout</Text>
+    </View>
+  )
+}
+
+export default AppLayout
